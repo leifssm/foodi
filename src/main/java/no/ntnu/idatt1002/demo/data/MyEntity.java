c0649d9ecd01a048de8eb7b3483cabc029536d66
Leif Mørstad <leif@moerstad.no>
2024-01-15 08:39:55 +0100

    updated indents

--- src/main/java/no/ntnu/idatt1002/demo/data/MyEntity.java
+++ src/main/java/no/ntnu/idatt1002/demo/data/MyEntity.java
@@ -5,33 +5,34 @@ import java.util.List;
 
 /**
  * This is just a simple Java-bean
+ *
  * @author nilstes
  */
 public class MyEntity {
-    private String id;
-    private String name;
-    
-    public MyEntity() {
-    }
+  private String id;
+  private String name;
 
-    public MyEntity(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+  public MyEntity() {
+  }
 
-    public String getName() {
-        return name;
-    }
+  public MyEntity(String id, String name) {
+    this.id = id;
+    this.name = name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
 }
--- src/main/java/no/ntnu/idatt1002/demo/repo/MyEntityRepo.java
+++ src/main/java/no/ntnu/idatt1002/demo/repo/MyEntityRepo.java
@@ -1,9 +1,9 @@
 package no.ntnu.idatt1002.demo.repo;
 
-import no.ntnu.idatt1002.demo.data.MyEntity;
-
 import java.util.Arrays;
 import java.util.List;
+
+import no.ntnu.idatt1002.demo.data.MyEntity;
 
 /**
  * Repository for the MyEntity-entity
@@ -12,27 +12,25 @@ import java.util.List;
  */
 public class MyEntityRepo {
 
-    /**
-     * Get object with given id
-     *
-     * @param  id  the entity id
-     * @return     an instance of MyEntity
-     */
-    public MyEntity getMyEntity(String id) {
-		// Get connection (maybe use pool?)
-		// Do some SQL
-		// Return som real data
-		
-		return new MyEntity("id", "name");
-    }
+  /**
+   * Get object with given id
+   *
+   * @param  id  the entity id
+   * @return     an instance of MyEntity
+   */
+  public MyEntity getMyEntity(String id) {
+    //  Return som real data
 
-    public List<MyEntity> findMyEntities(String someParameter) {
-		// Get connection (maybe use pool?)
-		// Do some SQL
-		// Return som real data
-		
-		return Arrays.asList(new MyEntity("id1", "name1"), new MyEntity("id2", "name2"));
-    }
+    return new MyEntity("id", "name");
+  }
+
+  public List<MyEntity> findMyEntities(String someParameter) {
+    // Get connection (maybe use pool?)
+    // Do some SQL
+    // Return som real data
+
+    return Arrays.asList(new MyEntity("id1", "name1"), new MyEntity("id2", "name2"));
+  }
 	
 	public void addMyEntity(MyEntity obj) {
 		// Get connection (maybe use pool?)
