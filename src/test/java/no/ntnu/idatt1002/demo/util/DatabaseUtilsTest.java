95c1010df799d9924a5b1b1198344a148233e417
Markush <markunha@stud.ntnu.no>
2024-03-05 17:11:38 +0100

    Pushing latest code changes

--- src/test/java/no/ntnu/idatt1002/demo/util/DatabaseUtilsTest.java
+++ src/test/java/no/ntnu/idatt1002/demo/util/DatabaseUtilsTest.java
@@ -2,11 +2,13 @@ package no.ntnu.idatt1002.demo.util;
 
 import no.ntnu.idatt1002.demo.model.util.DatabaseUtils;
 import org.junit.jupiter.api.Test;
+import no.ntnu.idatt1002.demo.model.Ingredient;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Class for testing the database utilities
+ * This class represents the test class for the DatabaseUtils class
+ *
  * @version 0.1.0
  * @author Snake727
  */
@@ -15,12 +17,25 @@ public class DatabaseUtilsTest {
 
       @Test
       public void testInitializeDatabase() {
-        // Test that the initializeDatabase method returns a connection to the database
-        // Call the initializeDatabase method
-        // Check that the method returns a connection to the database
+            // Create an instance of DatabaseUtils
+            DatabaseUtils dbUtils = new DatabaseUtils();
 
-        assertNotNull(DatabaseUtils.initializeDatabase());
+            // Call the initializeDatabase method on the instance
+            dbUtils.initializeDatabase();
+
+            // Check if the database is initialized by inserting a new ingredient into the database
+            // and then checking if the ingredient is in the database
+
+            // Create an instance of Ingredient
+            Ingredient ingredient = new Ingredient(1, "Apple", Ingredient.IngredientUnit.PIECE, Ingredient.IngredientCategory.FRUIT);
+
+            // Insert the ingredient into the database
+            dbUtils.insertIngredient(ingredient);
+
+
+
+
+
+
       }
-
-      // Other test methods for database utilities can be added here
 }
