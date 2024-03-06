9ef88d82df783d2bf87d32e3ee81247f69940cde
Leif Mørstad <leif@moerstad.no>
2024-03-06 18:17:24 +0100

    added progress bar for the inventory view

---
+++ src/main/java/no/ntnu/idatt1002/view/components/inventorylist/InventoryListProgressBar.java
@@ -0,0 +1,55 @@
+package no.ntnu.idatt1002.view.components.inventorylist;
+
+import javafx.scene.control.ProgressBar;
+import no.ntnu.idatt1002.view.utils.CssUtils;
+
+import java.util.Date;
+
+public class InventoryListProgressBar extends ProgressBar implements CssUtils {
+  private boolean isFrozen = false;
+
+  public InventoryListProgressBar() {
+    super();
+    addClass("inventory-list-progress-bar");
+    addStylesheet("inventory-list-progress-bar");
+
+    updateClasses();
+  }
+
+  public InventoryListProgressBar(Date expiry) {
+    this();
+    setExpiry(expiry);
+  }
+
+  public void setExpiry(Date expiry) {
+    double maxProgressInDays = 14;
+    double daysUntilExpired = 1 + (
+        (double) (expiry.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)
+    );
+
+    double progress = Math.max(0, Math.min(daysUntilExpired / maxProgressInDays, 1));
+
+    setProgress(progress);
+    updateClasses();
+  }
+
+  public void setIsFrozen(boolean isFrozen) {
+    this.isFrozen = isFrozen;
+  }
+
+  private void updateClasses() {
+    getStyleClass().removeAll("red", "yellow", "blue", "green");
+
+    double progress = getProgress();
+
+    if (isFrozen) {
+      getStyleClass().add("blue");
+    } else if (progress < 0.25) {
+      getStyleClass().add("red");
+    } else if (progress < 0.5) {
+      getStyleClass().add("yellow");
+    } else {
+      getStyleClass().add("green");
+    }
+  }
+}
