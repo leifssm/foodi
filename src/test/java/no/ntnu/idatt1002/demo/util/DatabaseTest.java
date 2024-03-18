6bc3ab51f89febfdd6d228f0df293c651ef51971
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2024-03-18 15:59:05 +0100

    endring av shoppinglist struktur

--- src/test/java/no/ntnu/idatt1002/demo/util/DatabaseTest.java
+++ src/test/java/no/ntnu/idatt1002/demo/util/DatabaseTest.java
@@ -3,6 +3,12 @@ package no.ntnu.idatt1002.demo.util;
 import no.ntnu.idatt1002.demo.model.repository.Database;
 import org.junit.jupiter.api.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static no.ntnu.idatt1002.demo.model.repository.Database.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
@@ -11,6 +17,25 @@ public class DatabaseTest {
       @BeforeEach
       public void setUp() {
             database = new Database();
+            String deleteInventorySql = "DELETE FROM inventory";
+            String deleteShoppingListSql = "DROP TABLE shopping_list";
+            String deleteUserSql = "DELETE FROM TEST.PUBLIC.\"user\"";
+            String deleteIngredientSql = "DELETE FROM ingredient";
+            String deleteRecipeSql = "DELETE FROM recipe";
+            String deleteRecipe_IngredientSql = "DELETE FROM recipe_ingredient";
+
+
+            try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS)){
+                  Statement statement = connection.createStatement();
+                  statement.executeUpdate(deleteInventorySql);
+                  statement.executeUpdate(deleteShoppingListSql);
+                  statement.executeUpdate(deleteUserSql);
+                  statement.executeUpdate(deleteIngredientSql);
+                  statement.executeUpdate(deleteRecipeSql);
+                  statement.executeUpdate(deleteRecipe_IngredientSql);
+            } catch (SQLException e) {
+                  System.out.println(e.getMessage());
+            }
       }
 
       @Test
