0fd9f62e6571b6fe08f0e0d6057c938fab7025c5
henrik392 <44232546+henrik392@users.noreply.github.com>
2024-03-19 14:15:02 +0100

    JavaDoc error

--- src/main/java/no/ntnu/idatt1002/view/utils/FontLoader.java
+++ src/main/java/no/ntnu/idatt1002/view/utils/FontLoader.java
@@ -15,72 +15,71 @@ import java.net.URI;
  */
 public final class FontLoader {
 
-    private static final int DEFAULT_FONT_SIZE = 12;
+  private static final int DEFAULT_FONT_SIZE = 12;
 
-    private FontLoader() {
-        throw new AssertionError("Utility class");
+  private FontLoader() {
+    throw new AssertionError("Utility class");
+  }
+
+  /**
+   * Loads fonts from a directory.
+   *
+   * @param fontDirectoryName the name of the directory containing the fonts
+   */
+  public static void loadFontsFromDirectory(String fontDirectoryName) {
+    URI directoryPath = LoadUtils.getFontDirectoryURI(fontDirectoryName);
+    File[] fontFiles = getFiles(directoryPath);
+
+    for (File fontFile : fontFiles) {
+      try {
+        loadFont(fontFile);
+      } catch (Exception e) {
+        System.out.println("Error loading font: " + e.getMessage());
+      }
+    }
+  }
+
+  /**
+   * Loads a font from a file.
+   *
+   * @param fontFile the file to load the font from
+   */
+  private static void loadFont(File fontFile) {
+    if (fontFile == null) {
+      throw new IllegalArgumentException("Font file is null");
     }
 
-    /**
-     * Loads fonts from a directory.
-     *
-     * @param fontDirectoryName the name of the directory containing the fonts
-     */
-    public static void loadFontsFromDirectory(String fontDirectoryName) {
-        URI directoryPath = LoadUtils.getFontDirectoryURI(fontDirectoryName);
-        File[] fontFiles = getFiles(directoryPath);
-
-        for (File fontFile : fontFiles) {
-            try {
-                loadFont(fontFile);
-            } catch (Exception e) {
-                System.out.println("Error loading font: " + e.getMessage());
-            }
-        }
+    if (!fontFile.isFile()) {
+      throw new IllegalArgumentException("Font file is not a file: " + fontFile.getName());
     }
 
-    /**
-     * Loads a font from a file.
-     *
-     * @param fontFile the file to load the font from
-     */
-    private static void loadFont(File fontFile) {
-        if (fontFile == null) {
-            throw new IllegalArgumentException("Font file is null");
-        }
-
-        if (!fontFile.isFile()) {
-            throw new IllegalArgumentException("Font file is not a file: " + fontFile.getName());
-        }
-
-        if (!fontFile.getName().endsWith(".ttf") && !fontFile.getName().endsWith(".otf")) {
-            throw new IllegalArgumentException("Font file is neither a .ttf or .otf file: " + fontFile.getName());
-        }
-
-        Font.loadFont(fontFile.toURI().toString(), DEFAULT_FONT_SIZE);
+    if (!fontFile.getName().endsWith(".ttf") && !fontFile.getName().endsWith(".otf")) {
+      throw new IllegalArgumentException("Font file is neither a .ttf or .otf file: " + fontFile.getName());
     }
 
-    /**
-     * Gets the files in a directory.
-     *
-     * @param fontDirectoryName the name of the directory containing the fonts
-     * @param directoryURI      the URI of the directory containing the fonts
-     * @return the files in the directory
-     */
-    @NotNull
-    private static File[] getFiles(URI directoryURI) {
-        String fontDirectoryName = directoryURI.getPath();
+    Font.loadFont(fontFile.toURI().toString(), DEFAULT_FONT_SIZE);
+  }
 
-        File dir = new File(directoryURI);
-        if (!dir.isDirectory()) {
-            throw new IllegalArgumentException("Font directory is not a directory: " + fontDirectoryName);
-        }
+  /**
+   * Gets the files in a directory.
+   *
+   * @param directoryURI the URI of the directory containing the fonts
+   * @return the files in the directory
+   */
+  @NotNull
+  private static File[] getFiles(URI directoryURI) {
+    String fontDirectoryName = directoryURI.getPath();
 
-        File[] fontFiles = dir.listFiles();
-
-        if (fontFiles == null) {
-            throw new IllegalArgumentException("Font directory is empty: " + fontDirectoryName);
-        }
-        return fontFiles;
+    File dir = new File(directoryURI);
+    if (!dir.isDirectory()) {
+      throw new IllegalArgumentException("Font directory is not a directory: " + fontDirectoryName);
     }
+
+    File[] fontFiles = dir.listFiles();
+
+    if (fontFiles == null) {
+      throw new IllegalArgumentException("Font directory is empty: " + fontDirectoryName);
+    }
+    return fontFiles;
+  }
 }
