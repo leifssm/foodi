5cc10a3bb85b46f191073d5876e72d8fe499a97e
Leif Mørstad <leif@moerstad.no>
2024-03-20 11:17:58 +0100

    added more methods for componentutils

--- src/main/java/no/ntnu/idatt1002/view/utils/ComponentUtils.java
+++ src/main/java/no/ntnu/idatt1002/view/utils/ComponentUtils.java
@@ -11,7 +11,6 @@ import org.jetbrains.annotations.NotNull;
  */
 public interface ComponentUtils {
   ObservableList<String> getStylesheets();
-
   ObservableList<String> getStyleClass();
 
   /**
@@ -39,20 +38,73 @@ public interface ComponentUtils {
   }
 
   /**
-   * Adds a class name to the elements' css class list.
-   *
-   * @param className the classname to add
-   */
-  default void removeClass(@NotNull String className) {
-    getStyleClass().remove(className);
-  }
-
-  /**
    * Adds all class names to the elements' css class list.
    *
    * @param className the classnames to add
    */
   default void addClasses(@NotNull String... className) {
     getStyleClass().addAll(className);
+  }
+
+  /**
+   * Removes a class name to the elements' css class list.
+   *
+   * @param className the classname to remove
+   */
+  default void removeClass(@NotNull String className) {
+    getStyleClass().remove(className);
+  }
+
+  /**
+   * Removes class names to the elements' css class list.
+   *
+   * @param classNames the classnames to remove
+   */
+  default void removeClasses(@NotNull String... classNames) {
+    getStyleClass().removeAll(classNames);
+  }
+
+  /**
+   * Returns the elements' css class list.
+   *
+   * @return the elements' css class list
+   */
+  default ObservableList<String> getClasses() {
+    return getStyleClass();
+  }
+
+  /**
+   * Adds a class name to the elements' css class list if it is not present, otherwise it removes
+   * it.
+   *
+   * @param className the classname to toggle
+   * @return whether the class exists after the operation
+   */
+  default boolean toggleClass(String className) {
+    if (getClasses().contains(className)) {
+      removeClass(className);
+      return false;
+    } else {
+      addClass(className);
+      return true;
+    }
+  }
+
+  /**
+   * Adds a class name to the elements' css class list if the condition is true, otherwise it
+   * removes it.
+   *
+   * @param className the classname to toggle
+   * @param condition whether to add or remove the class
+   * @return the condition
+   */
+  default boolean toggleClass(String className, boolean condition) {
+    if (condition) {
+      addClass(className);
+      return true;
+    } else {
+      removeClass(className);
+      return false;
+    }
   }
 }
