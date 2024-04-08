d5f8cb83a91fb75f0fff49df53d81c751aef4c28
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2024-04-08 10:54:05 +0200

    Refactor IngredientRecipe name

--- src/main/java/no/ntnu/idatt1005/foodi/MyApp.java
+++ src/main/java/no/ntnu/idatt1005/foodi/MyApp.java
@@ -1,7 +1,5 @@
 package no.ntnu.idatt1005.foodi;
 
-import no.ntnu.idatt1005.foodi.view.MyWindow;
-
 /**
  * Use this class to start the application
  * @author nilstes
--- src/main/java/no/ntnu/idatt1005/foodi/model/DAO/RecipeIngredientDAO.java
+++ src/main/java/no/ntnu/idatt1005/foodi/model/DAO/RecipeIngredientDAO.java
@@ -1,6 +1,6 @@
 package no.ntnu.idatt1005.foodi.model.DAO;
 
-import no.ntnu.idatt1005.foodi.model.objects.Recipe_Ingredient;
+import no.ntnu.idatt1005.foodi.model.objects.RecipeIngredient;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,7 +18,7 @@ public class RecipeIngredientDAO {
   private static final String USER = "sa";
   private static final String PASS = "";
 
-  public void save(Recipe_Ingredient obj) throws SQLException {
+  public void save(RecipeIngredient obj) throws SQLException {
     String checkSql = "SELECT COUNT(*) FROM recipe_ingredient WHERE recipe_id = ? AND ingredient_id = ?";
 
     try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -48,7 +48,7 @@ public class RecipeIngredientDAO {
     }
   }
 
-  public void update(Recipe_Ingredient obj) {
+  public void update(RecipeIngredient obj) {
     String sql = "UPDATE recipe_ingredient SET amount = ? WHERE recipe_id = ? AND ingredient_id = ?";
 
     try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -64,7 +64,7 @@ public class RecipeIngredientDAO {
     }
   }
 
-  public void delete(Recipe_Ingredient obj) {
+  public void delete(RecipeIngredient obj) {
     String sql = "DELETE FROM recipe_ingredient WHERE recipe_id = ? AND ingredient_id = ?";
 
     try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -79,7 +79,7 @@ public class RecipeIngredientDAO {
     }
   }
 
-  public Recipe_Ingredient retrieve(Recipe_Ingredient obj) {
+  public RecipeIngredient retrieve(RecipeIngredient obj) {
     String sql = "SELECT * FROM recipe_ingredient WHERE recipe_id = ? AND ingredient_id = ?";
 
     try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -90,7 +90,7 @@ public class RecipeIngredientDAO {
       ResultSet rs = pstmt.executeQuery();
 
       if (rs.next()) {
-        return new Recipe_Ingredient(rs.getInt("recipe_id"), rs.getInt("ingredient_id"), rs.getDouble("amount"));
+        return new RecipeIngredient(rs.getInt("recipe_id"), rs.getInt("ingredient_id"), rs.getDouble("amount"));
       }
 
     } catch (SQLException e) {
