ad62a5905b029148acc99dbd4f3d9e00973d3826
henrik392 <44232546+henrik392@users.noreply.github.com>
2024-04-09 14:16:07 +0200

    Created standard dialog class.

--- src/main/java/no/ntnu/idatt1002/view/components/dialog/StandardDialog.java
+++ src/main/java/no/ntnu/idatt1002/view/components/dialog/StandardDialog.java
@@ -1,6 +1,11 @@
 package no.ntnu.idatt1002.view.components.dialog;
 
+import javafx.scene.Node;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import no.ntnu.idatt1002.view.utils.DialogCssUtils;
 
 /**
@@ -17,5 +22,33 @@ public class StandardDialog extends Dialog<Void> implements DialogCssUtils {
     super();
     addStylesheet("components/std-dialog");
     addClass("std-dialog");
+  }
+
+  /**
+   * Adds an OK button to the dialog.
+   */
+  public void addOkButton() {
+    ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+    getDialogPane().getButtonTypes().add(okButtonType);
+    Node okButton = getDialogPane().lookupButton(okButtonType);
+    okButton.getStyleClass().add("button-ok");
+    expandButton(okButton);
+  }
+
+  private void expandButton(Node button) {
+    // Does not currently work :((
+    HBox.setHgrow(button, Priority.ALWAYS);
+    button.maxWidth(Double.MAX_VALUE);
+  }
+
+  /**
+   * Adds a cancel button to the dialog.
+   */
+  public void addCancelButton() {
+    ButtonType cancelButtonType = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+    getDialogPane().getButtonTypes().add(cancelButtonType);
+    Node cancelButton = getDialogPane().lookupButton(cancelButtonType);
+    cancelButton.getStyleClass().add("button-cancel");
+    expandButton(cancelButton);
   }
 }
--- src/main/java/no/ntnu/idatt1002/view/views/Profiles.java
+++ src/main/java/no/ntnu/idatt1002/view/views/Profiles.java
@@ -3,7 +3,6 @@ package no.ntnu.idatt1002.view.views;
 import java.util.List;
 import no.ntnu.idatt1002.view.components.CenteredPage;
 import no.ntnu.idatt1002.view.components.profiles.ProfileItems;
-import no.ntnu.idatt1002.view.utils.CssUtils;
 
 /**
  * Class for displaying the inventory page.
@@ -11,15 +10,13 @@ import no.ntnu.idatt1002.view.utils.CssUtils;
  * @author Henrik Kvamme
  * @version 1.0
  */
-public class Profiles extends CenteredPage implements CssUtils {
+public class Profiles extends CenteredPage {
 
   /**
    * Constructor for the Profiles class.
    */
   public Profiles() {
     super();
-    addStylesheet("components/profiles/profiles");
-    addClass("profiles");
 
     List<String> profileNames = List.of("Leif", "Henrik", "Kevin", "Markus");
     ProfileItems profileItems = new ProfileItems(profileNames);
